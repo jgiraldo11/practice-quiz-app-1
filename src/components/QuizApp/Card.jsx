1e4554029a18ce163861b46b7a40ad88478f9732
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-07-06 10:40:54 -0400

    INITIAL set up

---
+++ src/components/QuizApp/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card() {
+
+    return (
+        <div className="card">
+        <p>1/4</p>
+        <h2>Questions</h2>
+        <p>options</p>
+        </div>
+    )
+}
---
+++ src/layout/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const fullYear = new Date().getFullYear();
+
+export default function () {
+
+        return (
+            <footer>
+                <p>&copy; {fullYear}</p>
+            </footer>
+        )
+
+}
